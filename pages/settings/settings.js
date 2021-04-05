ab490afd31dfbc149881bc9ac5a7e07766f815de
therealmal <therealmal23@gmail.com>
2021-04-05 14:24:17 +0300

    Loaded files from PC
    Added header menu
    Added discord webhook

---
+++ pages/settings/settings.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function(){
+    /* Webhook */
+    chrome.storage.local.get('discordWebhook', function(storage){
+        if (storage.discordWebhook){
+            document.querySelector('input#webhookInput').value = storage.discordWebhook;
+        }
+    });
+    document.querySelector("#discordSave").addEventListener('click', function(){
+        chrome.storage.local.set({'discordWebhook': document.querySelector("input#webhookInput").value}, function(){});
+        fetch(document.querySelector("input#webhookInput").value,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "content": null,
+                    "embeds": [{
+                        "title": "Successfully checked out!",
+                        "color": 7209090,
+                        "fields": [
+                            {
+                                "name": "Site",
+                                "value": "Test Website"
+                            },
+                            {
+                                "name": "Item",
+                                "value": "Test Item"
+                            },
+                            {
+                                "name": "Profile",
+                                "value": "||Test Profile||"
+                            },
+                            {
+                                "name": "Order",
+                                "value": "||#1488||"
+                            }
+                        ],
+                        "footer": {
+                            "text": "TheRealMal EXT",
+                            "icon_url": "https://i.imgur.com/Csera95.png"
+                        },
+                        "thumbnail": {
+                            "url": "https://i.imgur.com/Csera95.png"
+                        }
+                    }]
+                })
+            }
+        );
+    });
+});
